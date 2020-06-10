eccb34b1708f7b6e1ae5583b67b9e77e735dcc84
kaelaclement <stephanie.kaela@gmail.com>
2020-06-10 16:23:22 +0100

    separate user routes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 
-const User = require('./models/userModel');
+const userRouter = require('./routes/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -33,63 +33,6 @@ app.get('/', (req, res) => {
 	res.render('index');
 });
 
-app.get('/signup', (req, res) => {
-	res.render('signup');
-});
-
-app.get('/login', (req, res) => {
-	res.render('login');
-});
-
-app.post('/login', async (req, res) => {
-	let { userName, password } = req.body;
-	if (!userName || !password) {
-		res.render('login', { err: 'Please fill out all fields' });
-		return;
-	}
-
-	let user = await User.findOne({ userName: userName });
-	if (user && password == user.password) {
-		req.session.user = user.id;
-		res.redirect(`/profile/${userName}`);
-	} else if (user) {
-		res.render('login', { err: 'Incorrect password' });
-	} else {
-		res.render('login', { err: 'User not found' });
-	}
-});
-
-app.get('/profile/:username', async (req, res) => {
-	let user = await User.findOne({ userName: req.params.username });
-	if (user) {
-		res.render('profile', { user: user.toObject() });
-	} else {
-		res.render('index', { err: 'User not found' });
-	}
-});
-
-app.post('/signup', async (req, res) => {
-	let { name, userName, email, password } = req.body;
-	if (!name || !userName || !email || !password) {
-		res.render('signup', { err: 'Please provide all credentials' });
-		return;
-	}
-
-	const user = new User({
-		name: name,
-		userName: userName,
-		email: email,
-		password: password
-	});
-
-	// await user.save()
-	user.save()
-		.then(success => res.redirect(`/profile/${userName}`))
-		.catch(error => {
-			res.render('signup', { err: 'A user with that username or email already exists' });
-		});
-
-	// res.redirect(`/profile/${req.body.userName}`);
-});
+app.use('/', userRouter);
 
 app.listen(port, () => console.log(`listening on port ${port}`));
